00be9dc5e84fb61379cbb86799c5660398ad160a
Josuecastro105 <jacoxd504@gmail.com>
2026-02-12 12:41:40 -0600

    Add files via upload
    Explicación de Colección de estructuras

---
+++ EstructuraCollection.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ public class EstructuraCollection {
+ 
+    public static void main(String[] args) {
+        
+        // declaracion de valores 
+        Collection<String> 
+        miColeccion = new ArrayList<>();
+
+        //asignar / agregar valores 
+        
+        miColeccion.add("elemento A");
+         
+        //eliminar valores 
+
+        miColeccion.remove ("elemento A");
+    }
+ }
